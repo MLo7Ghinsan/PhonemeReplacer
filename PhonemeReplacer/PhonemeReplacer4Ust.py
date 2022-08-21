1a089651eb7a5799bbf756f51a8bfcd59e2862c7
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2022-08-21 02:26:53 -0500

    Fixed encoding problem
    Shout out to haru0l for noticing hahhshqhqhshhqa

--- PhonemeReplacer/PhonemeReplacer4Ust.py
+++ PhonemeReplacer/PhonemeReplacer4Ust.py
@@ -11,11 +11,11 @@ for file in glob.glob("*.ust"):
     base = os.path.splitext(file)[0]
     os.rename(file, base + ".txt")
 
-for filepath in glob.iglob('./**/*.txt', recursive=True):
+for filepath in glob.iglob("./**/*.txt", recursive=True):
     with open(filepath) as file:
         s = file.read()
     s = s.replace(old_lyric, new_lyric)
-    with open(filepath, "w") as file:
+    with open(filepath, "w", encoding="utf-8", errors="ignore") as file:
         file.write(s)
 
 for file in glob.glob("*.txt"):
